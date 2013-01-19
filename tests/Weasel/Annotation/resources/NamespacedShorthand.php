ba623be95857ce41a0a6c83a01a3b9b42adfa8a8
Jonathan Oddy <jonathan@woaf.net>
2013-01-19 15:43:41 +0000

    Resolves php-weasel-23 Annontations aren't picked up if the class isn't in a namespace
    Added test cases for the PhpParser.
    Fixed bugs in PhpParser:
       * Fully qualified use statements were unusable. Suspect this was the actual problem originally reported.
       * Ignore comments and docblock comments.
       * Multiple use shorthand syntax support didn't work and led to none of the use statements  being picked up.
       * Fixed case where multiple namespace declarations appeared on the same line as the class declarations. This corner case probably wasn't really worth fixing, but nevermind.

---
+++ tests/Weasel/Annotation/resources/NamespacedShorthand.php
@@ -0,0 +1,16 @@
+<?php
+namespace Weasel\Annotation\TestResources;
+
+use /** @noinspection PhpUndefinedNamespaceInspection */
+    /** @noinspection PhpUndefinedClassInspection */
+    Weasel\Annotation\TestResources\TestD, /** @noinspection PhpUndefinedNamespaceInspection */
+    /** @noinspection PhpUndefinedClassInspection */
+    Weasel\Annotation\TestResources\TestF as Test, /** @noinspection PhpUndefinedNamespaceInspection */
+    /** @noinspection PhpUndefinedClassInspection */
+    Weasel\Annotation\TestResources\TestE;
+
+class NamespacedShorthand
+{
+
+}
+
